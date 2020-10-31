7672808b68e1db577fb90b02ad1aa8e0fbf6ac77
yoel <67129044+yoel0@users.noreply.github.com>
2020-10-30 23:12:38 -0700

    [add] save highscores in localStorage

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
 finalScore.innerText = mostRecentScore;
 
 
@@ -10,6 +13,17 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = (e) => {
-  console.log("clicked the save button!");
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort( (a, b) => b.score - a.score);
+  // if b is higher thea a then place b before a [scoresTop5]
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
 };
